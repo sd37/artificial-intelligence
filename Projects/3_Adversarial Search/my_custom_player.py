72b9da4c0978ecec128a96f4bafaf3ebbf336e32
spdutta <spdutta@microsoft.com>
2021-03-01 19:01:23 -0800

    90

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,4 +1,4 @@
-
+import random
 from sample_players import DataPlayer
 
 
@@ -9,38 +9,67 @@ class CustomPlayer(DataPlayer):
     You can modify the interface for get_action by adding named parameters
     with default values, but the function MUST remain compatible with the
     default interface.
-
-    **********************************************************************
-    NOTES:
-    - The test cases will NOT be run on a machine with GPU access, nor be
-      suitable for using any other machine learning techniques.
-
-    - You can pass state forward to your agent on the next turn by assigning
-      any pickleable object to the self.context attribute.
-    **********************************************************************
     """
+    
     def get_action(self, state):
-        """ Employ an adversarial search technique to choose an action
-        available in the current state calls self.queue.put(ACTION) at least
+        """ Choose an action available in the current state
+
+        See RandomPlayer and GreedyPlayer for examples.
 
         This method must call self.queue.put(ACTION) at least once, and may
-        call it as many times as you want; the caller will be responsible
-        for cutting off the function after the search time limit has expired.
-
-        See RandomPlayer and GreedyPlayer in sample_players for more examples.
+        call it as many times as you want; the caller is responsible for
+        cutting off the function after the search time limit has expired. 
 
         **********************************************************************
-        NOTE: 
-        - The caller is responsible for cutting off search, so calling
-          get_action() from your own code will create an infinite loop!
-          Refer to (and use!) the Isolation.play() function to run games.
+        NOTE: since the caller is responsible for cutting off search, calling
+              get_action() from your own code will create an infinite loop!
+              See (and use!) the Isolation.play() function to run games.
         **********************************************************************
         """
-        # TODO: Replace the example implementation below with your own search
-        #       method by combining techniques from lecture
-        #
-        # EXAMPLE: choose a random move without any search--this function MUST
-        #          call self.queue.put(ACTION) at least once before time expires
-        #          (the timer is automatically managed for you)
-        import random
-        self.queue.put(random.choice(state.actions()))
+        # randomly select a move as player 1 or 2 on an empty board, otherwise
+        # return the optimal minimax move at a fixed search depth of 3 plies
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            self.queue.put(self.minimax(state, depth=3))
+
+    def minimax(self, state, depth):
+
+        def min_value(state, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("inf")
+            for action in state.actions():
+                value = min(value, max_value(state.result(action), depth - 1))
+            return value
+
+        def max_value(state, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("-inf")
+            for action in state.actions():
+                value = max(value, min_value(state.result(action), depth - 1))
+            return value
+
+        return max(state.actions(), key=lambda x: min_value(state.result(x), depth - 1))
+
+    def score(self, state):
+        return self.custom_score(state) 
+    
+    def original_score(self, state):
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_liberties = state.liberties(own_loc)
+        opp_liberties = state.liberties(opp_loc)
+        return len(own_liberties) - len(opp_liberties)
+    
+    def custom_score(self, state):
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_liberties = state.liberties(own_loc)
+        opp_liberties = state.liberties(opp_loc)
+
+        utility_score = state.utility(self.player_id)
+        normal_score = int(len(own_liberties) - 1.2 * len(opp_liberties))
+        net_score = utility_score + normal_score
+        return net_score
